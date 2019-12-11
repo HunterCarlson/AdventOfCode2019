7ed2db6c93c3b5e03627b776c1ced865a8630c8c
Hunter Carlson <carlson.hunter@gmail.com>
2019-12-11 16:18:20 -0700

    Day3 Part2

--- AdventOfCode2019/Day3.cs
+++ AdventOfCode2019/Day3.cs
@@ -18,7 +18,10 @@ namespace AdventOfCode2019
             //Test1();
 
             Part1();
-            //Part2();
+
+            //Test2();
+
+            Part2();
         }
 
         private static void Test1()
@@ -27,24 +30,41 @@ namespace AdventOfCode2019
             List<Point> wire2 = GetWirePath("U7,R6,D4,L4");
 
             IEnumerable<Point> intersections = wire1.Intersect(wire2, new PointEqualityComparer());
-            int minDistance = intersections.Select(p => p.ManhattanDistance()).Where(d => d != 0).OrderBy(x => x).First();
+            int minDistance = intersections.Select(p => p.ManhattanDistance()).Where(d => d != 0).Min();
         }
 
         private static void Part1()
         {
             (List<Point> wire1, List<Point> wire2) = GetWirePaths();
             IEnumerable<Point> intersections = wire1.Intersect(wire2, new PointEqualityComparer());
-            int minDistance = intersections.Select(p => p.ManhattanDistance()).Where(d => d != 0).OrderBy(x => x).First();
+            int minDistance = intersections.Select(p => p.ManhattanDistance()).Where(d => d != 0).Min();
 
             Console.WriteLine("Part 1:");
             Console.WriteLine($"Distance: {minDistance}");
             Console.WriteLine();
         }
 
+        private static void Test2()
+        {
+            List<Point> wire1 = GetWirePath("R75,D30,R83,U83,L12,D49,R71,U7,L72");
+            List<Point> wire2 = GetWirePath("U62,R66,U55,R34,D71,R55,D58,R83");
+
+            IEnumerable<Point> intersections = wire1.Intersect(wire2, new PointEqualityComparer());
+
+            int minSteps = intersections.Select(i =>
+                wire1.First(w => w.X == i.X && w.Y == i.Y).Steps + wire2.First(w => w.X == i.X && w.Y == i.Y).Steps).Where(s => s != 0).Min();
+        }
+
         private static void Part2()
         {
+            (List<Point> wire1, List<Point> wire2) = GetWirePaths();
+            IEnumerable<Point> intersections = wire1.Intersect(wire2, new PointEqualityComparer());
+
+            int minSteps = intersections.Select(i =>
+                wire1.First(w => w.X == i.X && w.Y == i.Y).Steps + wire2.First(w => w.X == i.X && w.Y == i.Y).Steps).Where(s => s != 0).Min();
+
             Console.WriteLine("Part 2:");
-            Console.WriteLine("");
+            Console.WriteLine($"Steps: {minSteps}");
             Console.WriteLine();
         }
 
@@ -63,7 +83,8 @@ namespace AdventOfCode2019
             var points = new List<Point>();
             int x = 0;
             int y = 0;
-            points.Add(new Point(x, y));
+            int steps = 0;
+            points.Add(new Point(x, y, steps));
 
             string[] directions = dirStr.Split(',');
 
@@ -78,7 +99,8 @@ namespace AdventOfCode2019
                         foreach (int d in Enumerable.Range(0, distance))
                         {
                             y++;
-                            points.Add(new Point(x, y));
+                            steps++;
+                            points.Add(new Point(x, y, steps));
                         }
 
                         break;
@@ -88,7 +110,8 @@ namespace AdventOfCode2019
                         foreach (int d in Enumerable.Range(0, distance))
                         {
                             y--;
-                            points.Add(new Point(x, y));
+                            steps++;
+                            points.Add(new Point(x, y, steps));
                         }
 
                         break;
@@ -98,7 +121,8 @@ namespace AdventOfCode2019
                         foreach (int d in Enumerable.Range(0, distance))
                         {
                             x--;
-                            points.Add(new Point(x, y));
+                            steps++;
+                            points.Add(new Point(x, y, steps));
                         }
 
                         break;
@@ -108,7 +132,8 @@ namespace AdventOfCode2019
                         foreach (int d in Enumerable.Range(0, distance))
                         {
                             x++;
-                            points.Add(new Point(x, y));
+                            steps++;
+                            points.Add(new Point(x, y, steps));
                         }
 
                         break;
@@ -127,10 +152,19 @@ namespace AdventOfCode2019
         {
             X = x;
             Y = y;
+            Steps = 0;
+        }
+
+        public Point(int x, int y, int steps)
+        {
+            X = x;
+            Y = y;
+            Steps = steps;
         }
 
         public int X { get; set; }
         public int Y { get; set; }
+        public int Steps { get; set; }
 
         public int ManhattanDistance()
         {
